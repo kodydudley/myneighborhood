cdd362941f9c2b91bde2538cdf7e03dcd6514e2f
milesfwilson <milesfwilson@gmail.com>
2020-10-30 18:32:06 -0600

    adjusting style

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -16,12 +16,19 @@ export default class Post {
     
 
     <div class="row m-3 shadow-lg p-3 radius-25">
-    <div class="col-12 text-center">
+    <div class="col-12">
     <button class="btn btn-danger close" onclick="app.postsController.deletePost('${this.postId}')">x</button>
     
     <img class="img-fluid" src="${this.imgUrl}" alt=""/>
     <h3>${this.caption}</h3>
-    <form onsubmit="app.commentsController.addComment(event, '${this.postId}')">
+    <button class="bg-transparent text-dark border-dark radius-25 p-2" type="button" data-toggle="collapse" data-target="#content-${this.postId}" aria-expanded="false"
+        aria-controls="contentId">
+      Add comment
+    </button>
+
+    <div class="collapse" id="content-${this.postId}">
+
+    <form class="py-3" onsubmit="app.commentsController.addComment(event, '${this.postId}')">
     <div class="form-group">
     
     <input type="text" name="content" id="" class="radius-25 border-dark p-2 " placeholder="Add a comment" aria-describedby="helpId">
@@ -29,6 +36,13 @@ export default class Post {
     <button type="submit" class="btn btn-primary radius-25">+</button>
     </div>
     </form>
+
+    </div>
+
+
+
+  
+
     <div class="col-12" id="comments${this.postId}">
     ${this.Comments}
     </div>
