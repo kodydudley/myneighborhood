98438be68355dc111096a303391dea4611ced6d3
Kody Dudley <kodymdudley@gmail.com>
2020-10-30 20:49:51 -0600

    Comment Changes

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
-const ObjectId = mongoose.Types.ObjectId
+const ObjectId = Schema.Types.ObjectId
 
 // constructor(data) {
 //   this.commentId = data.commentId
